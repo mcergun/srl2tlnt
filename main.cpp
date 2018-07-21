3996763bc36797cffa58626a0cc3923f1c5fc7a6
Mert Can Ergun <mcergun90@gmail.com>
2018-07-21 21:56:46 +0300

    started using new serial library

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
-#include <srllib/rs232.h>
+#include <string>
+#include <serial/Serial.h>
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	const std::string deviceName = "/dev/pts/3";
 
-	int ret = 0;
-	ret = comEnumerate();
+	Serial srl = Serial(deviceName, 115200, 8, 'N', 1);
 
-	std::cout << "Number of available ports " << ret << std::endl;
-
-	for (int i = 0; i < ret; ++i)
+	if (srl.Open() == 0)
 	{
-		std::cout << i << ", " << comGetInternalName(i) << std::endl;
+		std::cout << "Device is opened successfully" << std::endl;
+	}
+
+	while (1)
+	{
+		bool readSuccessful = false;
+		// char readChar = srl.ReadChar(readSuccessful);
+		// if (readSuccessful)
+		// {
+		// 	std::cout << readChar;
+		// }
+		char buf[1024];
+		int readLen = sizeof(buf);
+		char readChar = srl.ReadBuf(buf, readLen, readSuccessful);
+		if (readSuccessful)
+		{
+			std::cout << buf;
+			std::cout.flush();
+		}
+		delay(100);
+	}
+
+	if (srl.IsOpened())
+	{
+		std::cout << "Closing device" << std::endl;
+		srl.Close();
 	}
 
 	return 0;
